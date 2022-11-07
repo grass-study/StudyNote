1ab14a40e6709bc5dcd069760b037d553d83da4a
CINY4612 <ciy5364612@gmail.com>
2022-11-07 22:37:19 +0900

    FEAT #2: Spring Security Config 설정

---
+++ src/main/java/com/ciny/studynote/security/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ciny.studynote.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+        return http.build();
+    }
+}
