ff336da96a80d742e775ddc4aac631af18538b5a
CINY4612 <ciy5364612@gmail.com>
2022-11-07 21:29:22 +0900

    FEAT #2: Controller 구현을 위한 Entity, Dto 작성

---
+++ src/main/java/com/ciny/studynote/model/Product.java
@@ -0,0 +1,42 @@
+package com.ciny.studynote.model;
+
+import com.ciny.studynote.dto.ProductRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private int lprice;
+
+    @Column(nullable = false)
+    private int myprice;
+
+    // 관심 상품 생성 시 이용합니다.
+    public Product(ProductRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.image = requestDto.getImage();
+        this.link = requestDto.getLink();
+        this.lprice = requestDto.getLprice();
+        this.myprice = 0;
+    }
+}
