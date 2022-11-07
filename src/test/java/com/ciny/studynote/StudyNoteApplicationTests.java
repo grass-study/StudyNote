62d52d5faf8613da40d573437d3d249b9c22398d
CINY4612 <ciy5364612@gmail.com>
2022-11-07 14:38:13 +0900

    Project Setpup
    (cherry picked from commit 52e78e40c45cb4217795b085a58fb4264bc53b33)

---
+++ src/test/java/com/ciny/studynote/StudyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciny.studynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
